3759e8dbd3490a493fe0af98336fb1217918d49b
Auditi Kurnia Wijayanti <126484165+auditikurnia@users.noreply.github.com>
2024-03-06 10:38:34 +0700

    Update Project-Kel-Jatim.py

--- Project-Kel-Jatim.py
+++ Project-Kel-Jatim.py
@@ -14,7 +14,7 @@ def print_nama_kel_Jatim(mantee):
 # Contoh list nama orang
 daftar_nama = ["Pangestu Sandya Etniko Siagian",
                "Name",
-               "Name",
+               "Auditi Kurnia Wijayanti",
                "Name",
                "Name"]
 
